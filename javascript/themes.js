fd8af7f5ccda894d8acb297e5ed9d61831821fcb
sarah <sarah.annealice@gmail.com>
2023-03-03 00:48:20 -0400

    trying out changing themes w/ js

--- javascript/themes.js
+++ javascript/themes.js
@@ -1,16 +1,17 @@
-//
+/*
+author: sarah newman
+date: march 2, 2023
+
+most recent update: march 3, 2023
+*/
 
 /*theme toggle*/
-document.addEventListener('DOMContentLoaded', () => {
-    const themeStylesheet = document.getElementById('theme')
-    const themeToggle = document.getElementById('theme-toggle')
-    themeToggle.addEventListener('click', () => {
-      /*if it's light -> go dark*/
-      if (themeStylesheet.href.includes('light')) {
-        themeStylesheet.href = '../css/styles_night.css'
-      } else {
-        /*if it's dark -> go light*/
-        themeStylesheet.href = '../css/styles_day.css'
-      }
-    })
-  })
+function toggleTheme() {
+  var theme = document.getElementsByTagName('link')[0];
+
+  if (theme.getAttribute('href') == '../css/styles_day.css') {
+      theme.setAttribute('href', '../css/styles_night.css');
+  } else {
+      theme.setAttribute('href', '../css/styles_day.css');
+  }
+}
