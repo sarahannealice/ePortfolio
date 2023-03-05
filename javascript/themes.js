e663433b8eeeaeb68e47b1b98c8e74cfb0d6831c
sarahannealice <sarah.annealice@gmail.com>
2023-03-04 23:49:59 -0400

    localstorage works

--- javascript/themes.js
+++ javascript/themes.js
@@ -5,40 +5,18 @@ date: march 2, 2023
 most recent update: march 3, 2023
 */
 
+var theme = document.getElementById('theme');
 
-/*theme toggle -- https://stackoverflow.com/a/14293019*/
-/*
-function switchTheme(sheet) {
-  document.getElementById("theme").setAttribute("href", sheet);
+if (localStorage.getItem('theme')) {
+    theme.setAttribute('href', localStorage.getItem('theme'));
 }
 
-function initiate() {
-  var night = document.getElementById("night_mode");
-  var day = document.getElementById("day_mode");
-
-  night.onclick = function() { switchTheme("../css/styles_night.css") };
-  day.onclick = function() { switchTheme("../css/styles_day.css") };
-}
-
-window.onload = initiate;
-*/
+/*theme toggle -- https://stackoverflow.com/a/18368517*/
 
 function switchTheme() {
-  var a = document.getElementById("theme");
-  a.x = 'styles_night' == a.x ? 'styles_day' : 'styles_night';
-  a.href = a.x + '.css';
-}
+    var a = document.getElementById('theme');
+    a.x = 'css/styles_night' == a.x ? 'css/styles_day' : 'css/styles_night'; // short if
+    //let status = a.href = a.x + '.css';
 
-/*
-function toggle() {
-  var el1 = document.getElementById("style1"),
-      el2 = document.getElementById("style2");
-  if (el1.disabled === "disabled") {
-    el1.disabled = undefined;
-    el2.disabled = "disabled";
-  } else {
-    el1.disabled = "disabled";
-    el2.disabled = undefined;
-  }
+    localStorage.setItem('theme', (a.href = a.x + '.css'));
 }
-*/
